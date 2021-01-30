979cd68596a9aa70d590cba2abaa36cb0ccc0328
tootal <tootal@yeah.net>
2021-01-30 04:05:26 -0800

    fix bash and bookmgr

--- src/core/task.cpp
+++ src/core/task.cpp
@@ -328,8 +328,11 @@ QProcess *Task::setupProcess() const
         QStringList allArgs;
         process->setProgram(getOptionsShellExecutable());
         allArgs << getOptionsShellArgs();
-        allArgs << command;
-        allArgs << args;
+        if (shell == "bash") {
+            allArgs << (QStringList() << command << args).join(' ').replace("\"", "\\\"");
+        } else {
+            allArgs << command << args;
+        }
         process->setArguments(allArgs);
     } else if (getType() == "process") {
         process->setProgram(command);
@@ -337,18 +340,18 @@ QProcess *Task::setupProcess() const
     }
 
     // connect signal and slot
-    connect(process, &QProcess::errorOccurred,
-            this, [this](QProcess::ProcessError error) {
-        emit showOutput(tr("[Task %1 error occurred with code %2]").arg(getLabel(), QString::number(error)));
+    connect(process, &QProcess::started,
+            this, [this]() {
+        emit showOutput(tr("[Task %1 started]\n").arg(getLabel()));
     });
     connect(process, &QProcess::readyReadStandardOutput,
             this, [process, this]() {
         auto text = process->readAllStandardOutput();
         emit showOutput(textDecode(text));
     });
-    connect(process, &QProcess::started,
-            this, [this]() {
-        emit showOutput(tr("[Task %1 started]\n").arg(getLabel()));
+    connect(process, &QProcess::errorOccurred,
+            this, [this](QProcess::ProcessError error) {
+        emit showOutput(tr("[Task %1 error occurred with code %2]").arg(getLabel(), QString::number(error)));
     });
     connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
             this, [this, process](int exitCode) {
